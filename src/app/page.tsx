9759d6f43a465154dc91e80beb3b03fc94180fc5
Martijn prive <martijnprive@macbookpro.home>
2025-09-12 19:17:40 +0200

    bigger onboarding flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { supabase } from "@/lib/supabaseClient";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
 import {
   Card,
   CardContent,
